b1c48bc0eaf8856c50a60c31985d7532544944f3
Firdarosa <firdarosa2@gmail.com>
2021-06-28 10:27:56 +0700

    update profile

--- app/src/main/java/com/example/campuscanteen/profile.java
+++ app/src/main/java/com/example/campuscanteen/profile.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
@@ -19,8 +20,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
@@ -33,20 +37,25 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
 import com.google.firebase.storage.UploadTask;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.util.HashMap;
 
 public class profile extends AppCompatActivity {
-    TextView username,fullname,email,phone;
-    ImageView profileImage;
-    Button logout;
+    private TextView username,fullname,email,phone;
+    private ImageView profileImage;
+    private Button logout;
     private FloatingActionButton editImageBtn;
-    String userId, MyUri="";
-    CircleImageView circleImageView;
+    private CircleImageView circleImageView;
+    private String userId, myUri="";
+    private Uri imageUri;
 
-    ChipNavigationBar navigationBar;
+    private ChipNavigationBar navigationBar;
 
-    FirebaseAuth firebaseAuth;
-    FirebaseFirestore fstore;
-    StorageReference storageReference;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore fstore;
+    private StorageReference storageReference;
     private StorageTask uploadTask;
 
     @Override
@@ -65,9 +74,16 @@ public class profile extends AppCompatActivity {
 
         firebaseAuth= FirebaseAuth.getInstance();
         fstore = FirebaseFirestore.getInstance();
-        storageReference = FirebaseStorage.getInstance().getReference().child("user/"+userId);
-
         userId = firebaseAuth.getCurrentUser().getUid();
+        storageReference = FirebaseStorage.getInstance().getReference().child("user/profilePicture");
+        StorageReference storageRef = storageReference.child(userId+".jpg");
+        storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(profileImage);
+            }
+        });
+
         if (savedInstanceState==null){
             navigationBar.setItemSelected(R.id.homeSelected,true);
         }
@@ -108,7 +124,7 @@ public class profile extends AppCompatActivity {
         editImageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                CropImage.activity().setAspectRatio(1,1).start(profile.this);
             }
         });
 
@@ -126,38 +142,100 @@ public class profile extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == 1000){
-            if (resultCode == Activity.RESULT_OK){
-                Uri imageUri = data.getData();
-                profileImage.setImageURI(imageUri);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK && data != null){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            imageUri = result.getUri();
 
-                profileImageUpload(imageUri);
+            //profileImage.setImageURI(imageUri);
+            uploadImage();
 
-            }
+        } else {
+            Toast.makeText(this, "Error, Try again ", Toast.LENGTH_SHORT).show();
         }
 
     }
-    private void profileImageUpload(Uri imageUri) {
 
-        StorageReference fileRef = storageReference.child("profile.jpg");
-        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                Toast.makeText(profile.this, "Image Uploaded", Toast.LENGTH_SHORT).show();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(profile.this, "Upload Failed !", Toast.LENGTH_SHORT).show();
-            }
-        });
+    private void uploadImage() {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("set your profile");
+        progressDialog.setMessage("Please Wait, While we are setting your data ");
+        progressDialog.show();
+
+        if (imageUri != null){
+            final StorageReference fileRef = storageReference.child(userId+".jpg");
+            fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            Picasso.get().load(uri).into(profileImage);
+                            Toast.makeText(profile.this, "profile image changed ", Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                        }
+                    });
+                }
+            });
+
+
+
+//            uploadTask.continueWith(new Continuation() {
+//                @Override
+//                public Object then(@NonNull Task task) throws Exception {
+//                    if (!task.isSuccessful()){
+//                        throw task.getException();
+//                    }
+//                    return fileRef.getDownloadUrl();
+//                }
+//            });.addOnCompleteListener(new OnCompleteListener<Uri>() {
+//                @Override
+//                public void onComplete(@NonNull Task<Uri> task) {
+//                    if (task.isSuccessful()){
+//                        Uri downloadUri = task.getResult();
+//                        myUri = downloadUri.toString();
+//
+//
+//                        DocumentReference reference = fstore.collection("users").document(userId);
+//                        HashMap<String, Object> userMap = new HashMap<>();
+//                        userMap.put("imageUrl",myUri);
+//                        reference.update(userMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+//                            @Override
+//                            public void onSuccess(Void aVoid) {
+//
+//                                Toast.makeText(profile.this, "Profile Image Changed ", Toast.LENGTH_SHORT).show();
+//                                progressDialog.dismiss();
+//
+//                            }
+//                        });
+//
+//                    }
+//                }
+//            });
+        }
 
     }
 
     public void logout(){
-        FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-        finish();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage("Are you sure want to Logout ?");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
 
